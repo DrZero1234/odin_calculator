413aa90bc7cea1d4c6a6a4733d02bd64177e5a6c
M.Kristóf <drzero123456@gmail.com>
2022-03-31 19:19:13 +0200

    Basic operation functions added

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Test")
+// Basic operator functions
+
+const test_a = 7;
+const test_b = 3;
+
+const add = (a,b) => {
+    return Number(a) + Number(b)
+}
+
+const subtract = (a,b) => {
+    return Number(a) - Number(b)
+}
+
+const multiply = (a,b) => {
+    return Number(a) * Number(b)
+}
+
+const divide = (a,b) => {
+    return Number(a) / Number(b)
+}
+
+
+console.log(add(test_a,test_b))
+console.log(subtract(test_a,test_b))
+console.log(multiply(test_a,test_b))
+console.log(divide(test_a,test_b))
